3527fad8039eb0291279718060b5d936bbbdb777
Joy <joy@cargomar.in>
2018-12-27 17:31:32 +0530

    save

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -17,9 +17,9 @@ export class LoginComponent {
 
   ErrorExternalLogin: string = '';
 
-  errorMessageVersion: string = '1.246';
+  errorMessageVersion: string = '1.247';
         
-  software_version_string: string = '1.246';
+  software_version_string: string = '1.247';
   
   
   server_software_version_string: string = '';
