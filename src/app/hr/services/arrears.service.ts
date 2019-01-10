19be886fe9113bf506da2dde3b7a126729fc4a61
joy <joycok@gmail.com>
2019-01-10 16:34:04 +0530

    arrears changed

---
+++ src/app/hr/services/arrears.service.ts
@@ -0,0 +1,32 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Arrearsm } from '../models/Arrearsm';
+import { GlobalService } from '../../core/services/global.service';
+
+@Injectable()
+export class ArrearsService {
+
+  constructor(
+    private http2: HttpClient,
+    private gs: GlobalService) {
+  }
+
+  List(SearchData: any) {
+    return this.http2.post<any>(this.gs.baseUrl + '/api/Hr/Arrears/List', SearchData, this.gs.headerparam2('authorized'));
+  }
+
+  GetRecord(SearchData: any) {
+    return this.http2.post<any>(this.gs.baseUrl + '/api/Hr/Arrears/GetRecord', SearchData, this.gs.headerparam2('authorized'));
+  }
+
+  Save(Record: Arrearsm) {
+    return this.http2.post<any>(this.gs.baseUrl + '/api/Hr/Arrears/Save', Record, this.gs.headerparam2('authorized'));
+  }
+
+  LoadDefault(SearchData: any) {
+    return this.http2.post<any>(this.gs.baseUrl + '/api/Hr/Arrears/LoadDefault', SearchData, this.gs.headerparam2('authorized'));
+  }
+
+}
+
