a20cefbef109dd64f2d2642ef06b4b395b1b001e
Joy <joy@cargomar.in>
2019-06-06 19:53:07 +0530

    save

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -12,8 +12,8 @@ export class LoginComponent {
   errorMessage: string;
   ErrorExternalLogin: string = '';
 
-  errorMessageVersion: string = '1.302';
-  software_version_string: string = '1.302';
+  errorMessageVersion: string = '1.303';
+  software_version_string: string = '1.303';
   username: string = 'ADMIN';
   password: string = 'ADMIN';
   
