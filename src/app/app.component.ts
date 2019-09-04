740b8376d6dc232edd646385f747e3a13f406645
Joy <cargomarprojects@outlook.com>
2019-09-04 12:29:30 +0530

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,41 @@
 ﻿import { Component } from '@angular/core';
 
+import {
+    Event,
+    NavigationCancel,
+    NavigationEnd,
+    NavigationError,
+    NavigationStart,
+    Router
+} from '@angular/router';
+
+
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
 })
 export class AppComponent {
     title = 'Application Root Page';
+    loading = false;
+
+    constructor(private router: Router) {
+        this.router.events.subscribe((event: Event) => {
+            switch (true) {
+                case event instanceof NavigationStart: {
+                    this.loading = true;
+                    break;
+                }
+                case event instanceof NavigationEnd:
+                case event instanceof NavigationCancel:
+                case event instanceof NavigationError: {
+                    this.loading = false;
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        });
+
+    }
 }
