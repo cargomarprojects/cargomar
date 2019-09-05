be14370dd11d0fe227e4fccfb65761e846c3992c
Joy <cargomarprojects@outlook.com>
2019-09-05 18:57:45 +0530

    save

--- src/app/accounts/models/collection.ts
+++ src/app/accounts/models/collection.ts
@@ -3,9 +3,9 @@ export class collectionreport {
     row_color: string;
     desc1: string;
     desc2: string;
-    col1: number;
-    col2: number;
-    col3: number;
-    col4: number;
+    col1:string;
+    col2: string;
+    col3: string;
+    col4: string;
 }
   
