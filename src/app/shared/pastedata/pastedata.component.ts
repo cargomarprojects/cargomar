61db0a3ffea67fc1c22bf458850a001198ba8cc9
Joy <cargomarprojects@outlook.com>
2019-09-17 16:09:44 +0530

    save

--- src/app/shared/pastedata/pastedata.component.ts
+++ src/app/shared/pastedata/pastedata.component.ts
@@ -76,13 +76,13 @@ export class PasteDataComponent implements OnInit {
 
   open() {
     this.displayed = true;
-    this.modalref = this.modalService.open(this.content, { size: "sm", backdrop: 'static', keyboard: false });
+    //this.modalref = this.modalService.open(this.content, { size: "sm", backdrop: 'static', keyboard: false });
   }
 
   close() {
     if (this.displayed) {
       this.displayed = false;
-      this.modalref.close();
+//      this.modalref.close();
       if (this.CloseClicked != null)
         this.CloseClicked.emit(null);
     }
