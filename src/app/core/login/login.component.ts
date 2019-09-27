a9dbca035d17cd6f4e4a4f230e10373f612f2f94
Joy <joycok@gmail.com>
2019-09-27 12:28:56 +0530

    sa

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -12,8 +12,8 @@ export class LoginComponent {
   errorMessage: string;
   ErrorExternalLogin: string = '';
 
-  errorMessageVersion: string = '1.330';
-  software_version_string: string = '1.330';
+  errorMessageVersion: string = '1.331';
+  software_version_string: string = '1.331';
   
   username: string = 'ADMIN';
   password: string = 'ADMIN';
