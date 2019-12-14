10a7e5a9fbcca714f5fa5092e7dace4df5e06be4
joy <joycok@gmail.com>
2019-12-14 13:30:10 +0530

    sgexp

---
+++ src/app/accounts/models/dailyexpd.ts
@@ -0,0 +1,9 @@
+import { GlobalVariables } from '../../core/models/globalvariables';
+
+export class Dailyexpd {
+
+
+
+    rec_mode: string;
+    _globalvariables: GlobalVariables;
+}
---
+++ src/app/accounts/models/dailyexpm.ts
@@ -0,0 +1,9 @@
+import { GlobalVariables } from '../../core/models/globalvariables';
+
+export class Dailyexpm {
+
+
+
+    rec_mode: string;
+    _globalvariables: GlobalVariables;
+}
