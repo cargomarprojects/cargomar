36ed4551f2e8f28387c98d20a4078ccb018c2123
Joy <joycok@gmail.com>
2020-07-11 13:59:20 +0530

    wave

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -12,8 +12,8 @@ export class LoginComponent {
   errorMessage: string;
   ErrorExternalLogin: string = '';
 
-  errorMessageVersion: string = '1.371';
-  software_version_string: string = '1.371';
+  errorMessageVersion: string = '1.372';
+  software_version_string: string = '1.372';
   
   username: string = 'ADMIN';
   password: string = 'ADMIN';
