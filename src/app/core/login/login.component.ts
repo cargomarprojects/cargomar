08933085431157a2de6e67f24a3fba10d403f001
Joy <joycok@gmail.com>
2020-08-18 15:33:14 +0530

    saev

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -12,8 +12,8 @@ export class LoginComponent {
   errorMessage: string;
   ErrorExternalLogin: string = '';
 
-  errorMessageVersion: string = '1.389';
-  software_version_string: string = '1.389';
+  errorMessageVersion: string = '1.390';
+  software_version_string: string = '1.390';
 
   username: string = 'ADMIN';
   password: string = 'ADMIN';
