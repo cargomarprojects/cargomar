6465f89e1312bbacc6d2872d7f3ac5dbe3464018
Joy <joycok@gmail.com>
2021-03-08 12:34:30 +0530

    save

--- src/app/operations/billing/buyrate.component.ts
+++ src/app/operations/billing/buyrate.component.ts
@@ -875,7 +875,7 @@ export class BuyRateComponent {
       }
 
 
-      if (rec.jv_acc_code == '1105033' || rec.jv_acc_code == '1205030') {
+      if (rec.jv_acc_code == '1105033' || rec.jv_acc_code == '1205030'|| rec.jv_acc_code == '1105040') {
          Courier_Code_Found = true;
       }
 
