23b5d8f0174abb230759982d956091b497333400
Joy <joycok@gmail.com>
2021-03-08 15:49:34 +0530

    save

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -12,8 +12,8 @@ export class LoginComponent {
   errorMessage: string;
   ErrorExternalLogin: string = '';
 
-  errorMessageVersion: string = '1.435';
-  software_version_string: string = '1.435';
+  errorMessageVersion: string = '1.436';
+  software_version_string: string = '1.436';
 
   username: string = 'ADMIN';
   password: string = 'cpl2001*';
