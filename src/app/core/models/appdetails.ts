4ff3e4e874b85e483d114da504be6a1d4f60bcc8
Joy <joycok@gmail.com>
2021-09-01 11:45:11 +0530

    save

---
+++ src/app/core/models/appdetails.ts
@@ -0,0 +1,10 @@
+export class AppDetails {
+    user_appid : string ;
+    user_code : string ;
+    user_password : string;
+    user_company_code : string ;
+    user_branch_id : string ;
+    user_year_id : string ;
+    user_hide_menu : boolean;
+    user_logged_out : boolean ;
+}
