ecb07f56e7a3f973e05b73d46cc760729d5fb95d
joy <joycok@gmail.com>
2022-12-28 16:59:36 +0530

    news

--- src/app/marketing/models/newcustrpt.ts
+++ src/app/marketing/models/newcustrpt.ts
@@ -1,17 +1,17 @@
 
 import { GlobalVariables } from '../../core/models/globalvariables';
 export class NewCustReport {
-    sman_name:string;
-    cust_name:string;
-    cons_name:string;
-    both_nos:number;
-    clr_nos:number;
-    fwd_nos:number;
-    rec_category:string;
-    first_job_date:string;
-    last_job_date:string;
-    previous_job_date:string;
-    
+    sman_name: string;
+    cust_name: string;
+    cons_name: string;
+    both_nos: number;
+    clr_nos: number;
+    fwd_nos: number;
+    rec_category: string;
+    first_job_date: string;
+    last_job_date: string;
+    previous_job_date: string;
+    cust_created_date: string;
     _globalvariables: GlobalVariables;
 }
 
