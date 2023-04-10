c1a91983ed567a8572fb56e394f6fbc4556333e6
joy <joycok@gmail.com>
2023-04-10 16:40:51 +0530

    sh

--- src/app/report1/models/shipmentreport.ts
+++ src/app/report1/models/shipmentreport.ts
@@ -14,5 +14,6 @@ export class ShipmentReport {
     jexp_inv_amt: number;
     opr_sbill_no: string;
     opr_sbill_date: string;
+    branch_code: string;
     _globalvariables: GlobalVariables;
 }
