560969518f0739f9f837b8fd395efadd9517c030
Joy <joycok@gmail.com>
2023-07-25 06:28:40 +0530

    sae

--- src/app/core/services/global.service.ts
+++ src/app/core/services/global.service.ts
@@ -31,7 +31,7 @@ export class GlobalService {
   public appid = "";
   public reload_url = "";
 
-  public software_version_string: string = '1.574';
+  public software_version_string: string = '1.575';
 
   public baseLocalServerUrl: string = "http://localhost:8080";
   public baseUrl: string = "http://localhost:5000";
