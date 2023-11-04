0da1f5f50ec6f6534ccbb2ad262c7961aecf5761
joy <joycok@gmail.com>
2023-11-04 11:39:44 +0530

    arp

--- src/app/accounts/services/arap.service.ts
+++ src/app/accounts/services/arap.service.ts
@@ -57,7 +57,7 @@ export class ArApService {
   }
   
   GenerateAllInvoice(SearchData: any) {
-    return this.http2.post<any>(this.gs.baseUrl + '/api/Accounts/ArAp/GenerateAllInvoice', SearchData, this.gs.headerparam2('authorized'));
+    return this.http2.post<any>(this.gs.baseUrl + '/api/Accounts/Report/GenerateAllInvoice', SearchData, this.gs.headerparam2('authorized'));
   }
 }
 
