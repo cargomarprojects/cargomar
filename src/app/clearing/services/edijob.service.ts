fbc6dd86d6092c971ab6ccb5611d3333dfc18396
joy <joycok@gmail.com>
2024-01-19 11:22:51 +0530

    edj

--- src/app/clearing/services/edijob.service.ts
+++ src/app/clearing/services/edijob.service.ts
@@ -25,7 +25,7 @@ export class EdijobService {
   }
 
   FileList(SearchData: any) {
-    return this.http2.post<any>(this.gs.baseUrl + '/api/xml/InwardEdiFiles/List', SearchData, this.gs.headerparam2('authorized'));
+    return this.http2.post<any>(this.gs.baseUrl + '/api/xml/job/List', SearchData, this.gs.headerparam2('authorized'));
   }
 
   ImportData(SearchData: any) {
