eb92d69e3e570ed4ef2b00efa62fc7b92ce2ec2e
joy <joycok@gmail.com>
2024-02-07 17:12:41 +0530

    unk

--- src/app/master/models/jobunlock.ts
+++ src/app/master/models/jobunlock.ts
@@ -8,11 +8,13 @@ export class JobUnlock {
     ul_remarks: string;
     ul_comments: string;
     ul_remarks_id: string;
+    shipper_name: string;
+    consignee_name: string;
+    billto_name: string;
     rec_mode: string;
 
     rec_company_code: string;
     rec_branch_code: string;
-
     rec_created_by: string;
     rec_created_date: string;
 
