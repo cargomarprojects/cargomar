963a65ce755fe80c1399f49a73a9edcf3eb94307
joy <joycok@gmail.com>
2024-04-03 12:29:58 +0530

    emp

--- src/app/hr/emp/emp.component.ts
+++ src/app/hr/emp/emp.component.ts
@@ -102,7 +102,7 @@ export class EmpComponent {
     private mainService: EmpService,
     private location: Location,
     private route: ActivatedRoute,
-    private gs: GlobalService
+    public gs: GlobalService
   ) {
     this.page_count = 0;
     this.page_rows = 30;
