db4e26fab16d25b32a885188260786ead127c08d
Joy <joycok@gmail.com>
2024-07-24 17:22:49 +0530

    savse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,61 +1,62 @@
-﻿import { Component, OnDestroy , OnInit } from '@angular/core';
-import { ActivatedRoute, Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router  } from '@angular/router';
-import { GlobalService } from './core/services/global.service'; 
+﻿import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { GlobalService } from './core/services/global.service';
 
 @Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html',
+  selector: 'my-app',
+  templateUrl: './app.component.html',
 })
 export class AppComponent implements OnDestroy {
-    title = 'Application Root Page';
-    loading = false;
+  // edited : 24/07/2024 joy
+  title = 'Application Root Page';
+  loading = false;
 
-    sub : any;
+  sub: any;
 
-    constructor(
-        public gs: GlobalService,
-        private route : ActivatedRoute,
-        private router: Router) {
+  constructor(
+    public gs: GlobalService,
+    private route: ActivatedRoute,
+    private router: Router) {
 
 
-        this.sub =  this.router.events.subscribe((event: Event) => {
-            switch (true) {
-                case event instanceof NavigationStart: {
-                    this.loading = true;
-                    break;
-                }
-
-                case event instanceof NavigationEnd:
-                case event instanceof NavigationCancel:
-                case event instanceof NavigationError: {
-                    this.loading = false;
-                    break;
-                }
-                default: {
-                    break;
-                }
-            }
-        });
-    }
-
-    ngOnInit() {
-        this.gs.InitdefaultValues();
-        this.gs.reload_url =  window.location.pathname + window.location.search;
-
-        
-        this.gs.appid = this.gs.getURLParameter(window.location.search, 'appid');
-
-        console.log( 'appid ',this.gs.appid);
-
-        if ( this.gs.isBlank(this.gs.appid)) {
-            this.router.navigate(['login'], { replaceUrl: true });
-            return;
+    this.sub = this.router.events.subscribe((event: Event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
         }
-        this.router.navigate(['/reload']);
-    }
 
-    ngOnDestroy(){
-        this.sub.unsubscribe();
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.gs.InitdefaultValues();
+    this.gs.reload_url = window.location.pathname + window.location.search;
+
+
+    this.gs.appid = this.gs.getURLParameter(window.location.search, 'appid');
+
+    console.log('appid ', this.gs.appid);
+
+    if (this.gs.isBlank(this.gs.appid)) {
+      this.router.navigate(['login'], { replaceUrl: true });
+      return;
     }
+    this.router.navigate(['/reload']);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 
 }
