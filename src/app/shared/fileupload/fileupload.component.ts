9f136145d6657e32f1bd085a5e8d1cf718dd93d4
Ajith <ajithkvs@gmail.com>
2024-10-18 16:48:52 +0530

    save

--- src/app/shared/fileupload/fileupload.component.ts
+++ src/app/shared/fileupload/fileupload.component.ts
@@ -337,7 +337,8 @@ export class FileUploadComponent {
         },
         error => {
           this.loading = false;
-          alert('Failed');
+          alert(this.gs.getError(error));
+          // alert('Failed');
         }
       );
   }
