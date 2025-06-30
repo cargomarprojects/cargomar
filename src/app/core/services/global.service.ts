7c29ac98e06987f65515bf117562d2da24827f76
Joy <joycok@gmail.com>
2025-06-30 07:24:28 +0530

    svse

--- src/app/core/services/global.service.ts
+++ src/app/core/services/global.service.ts
@@ -34,7 +34,7 @@ export class GlobalService {
   public Topbar_Email = "";
   public Topbar_Mob = "";
 
-  public software_version_string: string = '1.703';
+  public software_version_string: string = '1.704';
 
   public baseLocalServerUrl: string = "http://localhost:8080";
   public baseUrl: string = "http://localhost:5000";
